a8a25bfcb5cec28436db66185a4bbf22931ba555
Moki Daniel <mokidnl@gmail.com>
2021-04-01 11:09:46 +0300

    ft(stack_implementation) implemented 2nd option reversed array(warm up)

--- classwork/01/reverse.js
+++ classwork/01/reverse.js
@@ -2,3 +2,16 @@ const a = [10,20,30,40,50,60]
 console.log(a)
 const reversed = a.reverse();
 console.log("reversed", reversed);
+
+//reverse option two
+function reverse() {
+  
+    // Original Array
+    const a = [100,90,80,70,60,50];
+     console.log(a);
+  
+    // Reversed array
+    const new_arr = a.reverse();
+    console.log(new_arr);
+}
+reverse();
